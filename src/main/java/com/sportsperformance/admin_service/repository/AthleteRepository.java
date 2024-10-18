a334d0d1dfb640d6ca225c3c1c31380269483b06
itsVaibhavSharma <itsvaibhavsharma007@gmail.com>
2024-10-18 18:51:10 +0530

    Revert "sample Event and Athlete Registration API (GET & POST) added"
    This reverts commit c4a9d2251e8fca8547587f854d955b54118fa7da.

--- src/main/java/com/sportsperformance/admin_service/repository/AthleteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportsperformance.admin_service.repository;
-
-import com.sportsperformance.admin_service.model.Athlete;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AthleteRepository extends JpaRepository<Athlete, Long> {
-}
